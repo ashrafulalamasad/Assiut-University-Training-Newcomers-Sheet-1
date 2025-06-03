8d83aa65fefd716f6f379e7753ca460e79c2c84c
Ashraful Alam <ashrafulalamyt@gmail.com>
2025-06-03 06:58:13 +0600

    Add implementation of the "Area of a Circle" program in C++

---
+++ E. Area of a Circle.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define tc ll t;cin>>t;while(t--)
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main()
+{
+    fast;
+    ld r, pi = 3.141592653;
+    cin >> r;
+    cout << fixed << setprecision(9) << pi * r * r << endl;
+    return 0;
+}
